86170e16da88bc74a206f2eef401a2b972a1ccce
F2893819 Emanuel Galhardo de Oliveira <ego@bb.com.br>
2022-07-29 09:24:05 -0300

    Novas consultas e ajuste IP.

---
+++ src/main/resources/consultas/create_function_qtd_ocorrencias.sql
@@ -0,0 +1,18 @@
+
+DELIMITER $$
+CREATE FUNCTION qtd_ocorrencias (tipoAeronave CHAR(70))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+ DECLARE qtd int;
+select 
+sum(a.aeronave_fatalidades_total) as qtd_fatalidades
+ from aeronave a
+ inner join ocorrencia o on(a.codigo_ocorrencia2 = o.codigo_ocorrencia2) 
+ where a.aeronave_tipo_veiculo = tipoAeronave
+ into qtd;
+ 
+ return qtd;
+ 
+END $$
+DELIMITER 
---
+++ src/main/resources/consultas/create_trigger_Tgr_inclui_ocorrencia.sql
@@ -0,0 +1,12 @@
+DELIMITER $
+
+CREATE TRIGGER Tgr_inclui_ocorrencia AFTER INSERT
+ON cenipa.aeronave
+FOR EACH ROW
+BEGIN
+INSERT INTO cenipa.ocorrencia_fatalidade_quantidade (ocorrencia_fatalidade_quantidade_tipo, ocorrencia_fatalidade_quantidade_qtd)
+VALUES 
+(new.aeronave_tipo_veiculo, (select qtd_ocorrencias(new.aeronave_tipo_veiculo) from dual));
+END$
+DELIMITER ;
+
