bf93dd9415617d9c12b10a9d9ebbec6642fed31b
Emanuel Galhardo <emanuel.df@gmail.com>
2022-07-29 09:56:03 -0300

    Refatoração do acesso ao BD.

--- src/main/java/fbd/cenipa/Main.java
+++ src/main/java/fbd/cenipa/Main.java
@@ -1,11 +1,6 @@
 package fbd.cenipa;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
