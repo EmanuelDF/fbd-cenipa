79b144a0afbe9a39e4668a03e6fce536f740ae71
Emanuel Galhardo <emanuel.df@gmail.com>
2022-08-02 21:50:27 -0300

    Scripts syntaxes updated.

--- src/main/resources/scripts_v2/create_view_quantidade_ocorrencias_por_tipo.sql
+++ src/main/resources/scripts_v2/create_view_quantidade_ocorrencias_por_tipo.sql
@@ -1,10 +1,10 @@
-CREATE VIEW view_sumario_ocorrencias_por_tipo AS
-    SELECT  a.aeronave_tipo_operacao    AS  tipo_operacao,
-            COUNT(o.codigo_ocorrencia2) AS  quantidade_ocorrencias
-    FROM    aeronave a
-                INNER JOIN ocorrencia o
-                           ON(o.codigo_ocorrencia2 = a.codigo_ocorrencia2)
-    GROUP BY a.aeronave_tipo_operacao
-    ORDER BY quantidade_ocorrencias DESC;
+CREATE VIEW view_quantidade_ocorrencias_por_tipo AS
+SELECT  a.aeronave_tipo_operacao    AS  tipo_operacao,
+        COUNT(o.codigo_ocorrencia2) AS  quantidade_ocorrencias
+FROM    aeronave a
+        INNER JOIN ocorrencia o
+        ON(o.codigo_ocorrencia2 = a.codigo_ocorrencia2)
+GROUP BY a.aeronave_tipo_operacao
+ORDER BY quantidade_ocorrencias DESC;
 
-SELECT * FROM view_sumario_ocorrencias_por_tipo;
+SELECT * FROM view_quantidade_ocorrencias_por_tipo;
