d12e27ac266bd7710c2e6d68a27ff847f64c87cc
Emanuel Galhardo <emanuel.df@gmail.com>
2022-08-02 22:07:57 -0300

    SQL scripts updated.

--- src/main/resources/scripts_v2/consulta_e_percentual_ocorrencias_por_tipo_operacao.sql
+++ src/main/resources/scripts_v2/consulta_e_percentual_ocorrencias_por_tipo_operacao.sql
@@ -6,8 +6,8 @@ SELECT  tipo_operacao,
                 TRUNCATE(quantidade_ocorrencias * 100 / total.soma, 2),
             '.', ','),
         '%') AS percentual
-FROM    view_sumario_ocorrencias_por_tipo
+FROM    view_quantidade_ocorrencias_por_tipo
     CROSS JOIN (
     SELECT SUM(quantidade_ocorrencias) AS soma
-    FROM view_sumario_ocorrencias_por_tipo) total
+    FROM view_quantidade_ocorrencias_por_tipo) total
 ORDER BY quantidade_ocorrencias DESC;
